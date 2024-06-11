2967241170a4196bb9dcc2dc579ef8b7a9c7508d
RakshitDev <rbalagundi@gmail.com>
2024-06-11 21:36:13 +0530

    findWareHouse_Created

--- src/main/java/com/jsp/whms/service/WareHouseService.java
+++ src/main/java/com/jsp/whms/service/WareHouseService.java
@@ -4,6 +4,7 @@ import org.springframework.http.ResponseEntity;
 
 import com.jsp.whms.entity.WareHouse;
 import com.jsp.whms.requestdto.WareHouseRequest;
+import com.jsp.whms.responsedto.AdminResponse;
 import com.jsp.whms.responsedto.WareHouseResponse;
 import com.jsp.whms.util.ResponseStructure;
 
@@ -12,5 +13,7 @@ public interface WareHouseService {
 public	ResponseEntity<ResponseStructure<WareHouseResponse>> createWareHouse(WareHouseRequest wareHouseRequest);
 
 public ResponseEntity<ResponseStructure<WareHouseResponse>> updateWareHouse(WareHouseRequest wareHouseRequest, int wareHouseId);
+
+public ResponseEntity<ResponseStructure<WareHouseResponse>> findWarehouse(int warehouseId);
 
 }
